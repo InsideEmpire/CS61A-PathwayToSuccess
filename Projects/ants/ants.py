b3bf3137b608142c0e285425263df2fd2c76c943
Garrison <garrison_liu@outlook.com>
2025-01-28 12:07:44 +0800

    finish Problem 8a

--- Projects/ants/ants.py
+++ Projects/ants/ants.py
@@ -314,12 +314,15 @@ class ContainerAnt(Ant):
 
     def can_contain(self, other):
         # BEGIN Problem 8a
-        "*** YOUR CODE HERE ***"
+        if not other.is_container and not self.ant_contained:
+            return True
+        else:
+            return False
         # END Problem 8a
 
     def store_ant(self, ant):
         # BEGIN Problem 8a
-        "*** YOUR CODE HERE ***"
+        self.ant_contained = ant
         # END Problem 8a
 
     def remove_ant(self, ant):
@@ -339,7 +342,8 @@ class ContainerAnt(Ant):
 
     def action(self, gamestate):
         # BEGIN Problem 8a
-        "*** YOUR CODE HERE ***"
+        if self.ant_contained:
+            self.ant_contained.action(gamestate)
         # END Problem 8a
 
 
