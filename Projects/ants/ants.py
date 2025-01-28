2cc805382e90f9610468eed5e86892af011a5975
Garrison <garrison_liu@outlook.com>
2025-01-28 15:08:16 +0800

    finish Problem 8b

--- Projects/ants/ants.py
+++ Projects/ants/ants.py
@@ -118,7 +118,13 @@ class Ant(Insect):
             place.ant = self
         else:
             # BEGIN Problem 8b
-            assert place.ant is None, 'Too many ants in {0}'.format(place)
+            if place.ant.is_container and place.ant.can_contain(self):
+                place.ant.store_ant(self)
+            elif self.is_container and self.can_contain(place.ant):
+                self.store_ant(place.ant)
+                place.ant = self
+            else:
+                assert place.ant is None, 'Too many ants in {0}'.format(place)
             # END Problem 8b
         Insect.add_to(self, place)
 
