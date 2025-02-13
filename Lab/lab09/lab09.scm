78b183a286120169c5ec25f9dfa098484e9d09d9
Garrison Lewe <garrison_liu@outlook.com>
2025-02-13 13:30:15 +0800

    finish Lab9 and Lab 8 (#20)
    * update lab8
    
    * finish Q1: WWPD: Trees and Q2: Cumulative Mul
    
    * finish Q3: Prune Small
    
    * finish Q4: Delete
    
    * finish Q5: Maximum Path Sum
    
    * update lab9
    
    * finish Q1: Over or Under
    
    * finish Q2: Make Adder
    
    * finish Q3: Compose
    
    * finish Q4: Repeat
    
    * finish Q5: Greatest Common Divisor

---
+++ Lab/lab09/lab09.scm
@@ -0,0 +1,48 @@
+(define (over-or-under num1 num2) 
+  (cond 
+    ((< num1 num2) -1)
+    ((< num2 num1) 1)
+    ((= num1 num2) 0)
+  )
+)
+
+(define (make-adder num) 
+  (define (adder inc)
+    (+ num inc)
+  )
+  adder
+)
+
+(define (composed f g) 
+  (define (compose_f_g num)
+    (f (g num))
+  )
+  compose_f_g
+)
+
+(define (repeat f n) 
+  (define (repeat_f x)
+    (if (< n 1)
+      x
+      ((repeat f (- n 1)) (f x))
+    )
+  )
+  repeat_f
+)
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b) 
+  (if (zero? (modulo (max a b) (min a b)))
+    (min a b)
+    (gcd (min a b) (modulo (max a b) (min a b)))
+  )
+)
